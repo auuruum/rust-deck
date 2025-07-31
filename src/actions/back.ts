5baa071de57a1285ad8311c08b94af5675177640
auuruum <aurimasdovgialo@gmail.com>
2025-07-31 19:25:29 +0300

    Made back fully working

--- src/actions/back.ts
+++ src/actions/back.ts
@@ -2,10 +2,11 @@ import streamDeck, { action, KeyDownEvent, SingletonAction } from "@elgato/strea
 
 @action({ UUID: "com.aurum.rust-deck.back" })
 export class Back extends SingletonAction {
-	/**
-	 * Occurs when the user presses the key action.
-	 */
-	override onKeyDown(ev: KeyDownEvent): void | Promise<void> {
-		streamDeck.profiles.switchToProfile(ev.action.device.id, "profiles/Smart Switches"); 
-	}
+    /**
+     * Occurs when the user presses the key action.
+     */
+    override onKeyDown(ev: KeyDownEvent): void | Promise<void> {
+        // Passing undefined as the profile name will switch back to the previous profile
+        streamDeck.profiles.switchToProfile(ev.action.device.id, undefined);
+    }
 }
