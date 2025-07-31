e9045677ae7ce32da7a723123314c6ff3ecfb414
auuruum <aurimasdovgialo@gmail.com>
2025-07-31 19:24:51 +0300

    Fixed

--- src/actions/back.ts
+++ src/actions/back.ts
@@ -1,14 +1,11 @@
-// src/actions/back.ts
 import streamDeck, { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
 
-// match the same in-memory store if you ever need it,
-// but here we’re doing a hard-coded back to "Default".
-type NoSettings = {};
-
 @action({ UUID: "com.aurum.rust-deck.back" })
-export class Back extends SingletonAction<NoSettings> {
-  override onKeyDown(ev: KeyDownEvent<NoSettings>): void {
-    // hard-coded “go back to Default”
-    streamDeck.profiles.switchToProfile(ev.action.device.id, "Default");
-  }
+export class Back extends SingletonAction {
+	/**
+	 * Occurs when the user presses the key action.
+	 */
+	override onKeyDown(ev: KeyDownEvent): void | Promise<void> {
+		streamDeck.profiles.switchToProfile(ev.action.device.id, "profiles/Smart Switches"); 
+	}
 }
