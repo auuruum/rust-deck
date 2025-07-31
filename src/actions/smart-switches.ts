875fe7dd2301a8172b998d523ed220815f54994a
auuruum <aurimasdovgialo@gmail.com>
2025-07-31 17:39:16 +0300

    Smart Switches button

---
+++ src/actions/smart-switches.ts
@@ -0,0 +1,15 @@
+import streamDeck, { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+
+@action({ UUID: "com.aurum.rust-deck.smart-switches" })
+export class SmartSwitches extends SingletonAction {
+	/**
+	 * Occurs when the user presses the key action.
+	 */
+	override onKeyDown(ev: KeyDownEvent<CounterSettings>): void | Promise<void> {
+		streamDeck.profiles.switchToProfile(ev.action.device.id, "Smart Switches"); 
+	}
+}
+
+type CounterSettings = {
+	count: number;
+};
--- src/plugin.ts
+++ src/plugin.ts
@@ -3,6 +3,7 @@ import { LogLevel, streamDeck } from "@elgato/streamdeck";
 import { TimeDisplay } from "./actions/time-display";
 import { ServerInfo } from "./actions/server-info";
 import { PhaseOfDay } from "./actions/phase-of-day";
+import { SmartSwitches } from "./actions/smart-switches";
 import { initializeGlobalSettings } from "./settings";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information (e.g. access tokens, etc.) make sure to disable logging so that the information isn't recorded in the log files.
@@ -19,6 +20,8 @@ streamDeck.actions.registerAction(new TimeDisplay());
 streamDeck.actions.registerAction(new ServerInfo());
 // Register the phase of day action
 streamDeck.actions.registerAction(new PhaseOfDay());
+// Register the smart switches action
+streamDeck.actions.registerAction(new SmartSwitches());
 
 // Connect to Stream Deck
 streamDeck.connect();
