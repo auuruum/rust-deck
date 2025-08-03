e81edc0b43ca27db8cb61e22cc6fbe71c9c61579
auuruum <aurimasdovgialo@gmail.com>
2025-08-03 06:32:05 +0300

    Added smart devices

---
+++ src/actions/smart-devices.ts
@@ -0,0 +1,18 @@
+import streamDeck, { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+
+@action({ UUID: "com.aurum.rust-deck.smart-devices" })
+export class SmartDevices extends SingletonAction {
+	/**
+	 * Occurs when the user presses the key action.
+	 */
+	override async onKeyDown(ev: KeyDownEvent): Promise<void> {
+		// Set global variable to indicate we want to show smart alarms
+		await streamDeck.settings.setGlobalSettings({
+			...(await streamDeck.settings.getGlobalSettings()),
+			profileType: "smart_devices"
+		});
+		
+		// Switch to the profile
+		streamDeck.profiles.switchToProfile(ev.action.device.id, "profiles/Rustplusplus"); 
+	}
+}
